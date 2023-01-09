675662537ac927faafbe028f8d930969cbb703c2
MagStrength <101830802+MagStrength@users.noreply.github.com>
2023-01-09 19:27:47 +0300

    7lesson

---
+++ 7lesson/src/pages/Page404/Page404.jsx
@@ -0,0 +1,6 @@
+
+export function Page404() {
+    return (
+        <h2>Page 404</h2>
+    )
+}
---
+++ 7lesson/src/store/profile/selectors.js
@@ -0,0 +1,7 @@
+export function getUserName(state) {
+    return state.profile.name
+}
+
+export function getNameVisible(state) {
+    return state.profile.visible
+}
